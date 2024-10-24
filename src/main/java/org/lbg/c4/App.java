7c920284cc8fff1e610e8461f17f0e2db0686334
Lonyin <hello@lonyinchan.com>
2024-10-24 15:21:00 +0100

    Done part 5

--- src/main/java/org/lbg/c4/App.java
+++ src/main/java/org/lbg/c4/App.java
@@ -14,27 +14,36 @@ public class App {
     static double totalCost = 0;
     private static final DecimalFormat decfor = new DecimalFormat("0.00");
 
-    private static ArrayList<Double> itemPrices = new ArrayList<>();
+    private static ArrayList<Item> items = new ArrayList<>();
     public static void main(String[] args){
+        Scanner myScanner = new Scanner(System.in);
+        String itemCost = "";
+        String itemQuantity = "";
 
         try {
-            Scanner myScanner = new Scanner(System.in);
-            String input = "";
-
             System.out.println("Enter VAT rate");
             double vat = Double.parseDouble(myScanner.nextLine());
 
             while (true) {
                 System.out.println("Enter item cost or quit (Enter QUIT): ");
-                input = myScanner.nextLine();
-                if (input.isEmpty()) {
+                itemCost = myScanner.nextLine();
+                if (itemCost.isEmpty()) {
                     continue;
                 }
-                if (input.equals("QUIT")) {
+                if (itemCost.equals("QUIT")) {
                     break;
                 }
-                addToArray(Double.parseDouble(input));
-                totalCost += totalPrice(Double.parseDouble(input), vat);
+
+                System.out.println("Enter item quantity or quit (Enter QUIT): ");
+                itemQuantity = myScanner.nextLine();
+                if (itemQuantity.isEmpty()) {
+                    continue;
+                }
+                if (itemQuantity.equals("QUIT")) {
+                    break;
+                }
+               
+                processItem(Double.parseDouble(itemCost), Integer.parseInt(itemQuantity), vat);
                 System.out.println("Current total cost: £" + decfor.format(totalCost));
             }
             myScanner.close();
@@ -42,23 +51,24 @@ public class App {
         catch (Exception e) {
             System.out.println(e);
         }
+        printResult();
+    }
 
+    public static void printResult() {
         System.out.println("Final total cost is: £" + decfor.format(totalCost));
-        Collections.sort(itemPrices);
         printPrices();
     }
 
     public static void printPrices() {
-        for (int i = 0; i < itemPrices.size(); i++) {
-            System.out.println("Item " + (i + 1) + " costs: £" + decfor.format(itemPrices.get(i)));
+        for (int i = 0; i < items.size(); i++) {
+            System.out.println("Item " + (i + 1) + " costs: £" + decfor.format(items.get(i).getTotalPrice()));
         }
     }
 
-    public static void addToArray(double price) {
-        itemPrices.add(price);
-    }
+    public static void processItem(double cost, int quantity, double vat) {
+        Item newItem = new Item(cost, quantity, vat);
+        items.add(newItem);
 
-    public static double totalPrice(double cost, double vat) {
-        return cost * (1 + vat);
+        totalCost += newItem.getTotalPrice();
     }
 }
--- src/main/java/org/lbg/c4/Item.java
+++ src/main/java/org/lbg/c4/Item.java
@@ -1,2 +1,17 @@
-package org.lbg.c4;public class Item {
+package org.lbg.c4;
+
+public class Item {
+    public double cost;
+    public int quantity;
+    public double vat;
+
+    public Item(double cost, int quantity, double vat) {
+        this.cost = cost;
+        this.quantity = quantity;
+        this.vat = vat;
+    }
+
+    public double getTotalPrice() {
+        return cost * (1+vat) * quantity;
+    }
 }
