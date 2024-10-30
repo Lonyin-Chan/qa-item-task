817e85f2bad4350d30589432a28ee400f44961e9
Lonyin Chan <hello@lonyinchan.com>
2024-10-30 16:59:21 +0000

    Third test done, two item ascending in array

--- src/main/java/tdd/HighestNumberFinder.java
+++ src/main/java/tdd/HighestNumberFinder.java
@@ -1,7 +1,17 @@
 package tdd;
 
+import java.util.Arrays;
+
 public class HighestNumberFinder {
     public int findHighestNumber(int[] input) {
-        return input[0];
+//        Arrays.sort(input);
+//        return input[input.length - 1];
+
+        int highestSoFar = input[0];
+
+        if (input.length > 1 && input[1] > highestSoFar)
+            highestSoFar = input[1];
+
+        return highestSoFar;
     }
 }
