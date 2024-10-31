5f40ac57c8d073d5a07112842fa640d2e1ad99f6
Lonyin Chan <hello@lonyinchan.com>
2024-10-31 14:17:31 +0000

    Created tests for MsgQuantityReader

---
+++ src/test/java/org/lbg/c4/MsgQuantityReaderTest.java
@@ -0,0 +1,73 @@
+package org.lbg.c4;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MsgQuantityReaderTest {
+
+    private MsgQuantityReader cut;
+
+    @BeforeEach
+    public void setUp() {
+        ICustomPrompt customPrompt = new CustomPrompt();
+        cut = new MsgQuantityReader(customPrompt);
+    }
+
+    @Test
+    public void readFromKeyboard_valid_input() {
+        String expectedResult = "22";
+        System.setIn(new ByteArrayInputStream("22".getBytes()));
+
+        String actualResult = cut.readFromKeyboard(System.in);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void readFromKeyboard_invalid_then_valid_input() {
+        String expectedResult = "22";
+        System.setIn(new ByteArrayInputStream("cheese".getBytes()));
+        System.setIn(new ByteArrayInputStream("22".getBytes()));
+
+        String actualResult = cut.readFromKeyboard(System.in);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void readFromKeyboard_quit_input() {
+        String expectedResult = "QUIT";
+        System.setIn(new ByteArrayInputStream("QUIT".getBytes()));
+
+        String actualResult = cut.readFromKeyboard(System.in);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void isValid_input_valid_value() {
+        boolean actualResult = cut.isValid("22");
+
+        assertTrue(actualResult);
+    }
+
+    @Test
+    public void isValid_input_invalid_value() {
+        boolean actualResult = cut.isValid("cheese");
+
+        assertFalse(actualResult);
+    }
+
+    @Test
+    public void isValid_input_quit_value() {
+        boolean actualResult = cut.isValid("QUIT");
+
+        assertTrue(actualResult);
+    }
+
+}
